e95f3c70c4e67b3e7aa58f6f830123a290683a37
srianish <rohithkumar8070@gmail.com>
2023-04-13 04:27:15 +0530

    ijijl

--- controllers/authorisation/forgotController.js
+++ controllers/authorisation/forgotController.js
@@ -1,16 +1,17 @@
-const jwt = require('jsonwebtoken');
-const nodemailer = require('nodemailer');
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const User = require("../../models/User");
 
 async function sendEmail(to, subject, html) {
   // Create a transporter object
   const transporter = nodemailer.createTransport({
-    host: 'smtp.example.com',
+    host: "smtp.example.com",
     port: 587,
     secure: false, // use TLS
     auth: {
-      user: 'user@example.com',
-      pass: 'password'
-    }
+      user: "user@example.com",
+      pass: "password",
+    },
   });
 
   // Define the email options
@@ -18,7 +19,7 @@ async function sendEmail(to, subject, html) {
     from: '"Your App" <noreply@example.com>',
     to,
     subject,
-    html
+    html,
   };
 
   // Send the email
@@ -26,40 +27,47 @@ async function sendEmail(to, subject, html) {
   console.log(`Email sent: ${info.messageId}`);
 }
 
-async function sendResetEmail(email) {
-  // Generate a reset password token
-  const resetToken = crypto.randomBytes(32).toString('hex');
-
-  // Save the reset password token in the database
-  await User.findOneAndUpdate({ email }, { resetPasswordToken: resetToken });
-
+async function sendResetEmail(email, resetToken) {
   // Send the reset password email to the user
   const resetUrl = `http://localhost:3000/reset-password?token=${resetToken}`;
   const message = `Click the link to reset your password: ${resetUrl}`;
-  await sendEmail(email, 'Reset your password', message);
+  await sendEmail(email, "Reset your password", message);
 }
 
-
-module.exports = function Forgot(req, res){
+module.exports = function Forgot(req, res) {
   // Validate the request body
   if (!req.body.email) {
-    return res.status(400).send({ error: 'Email is required' });
+    return res.status(400).send({ error: "Email is required" });
   }
 
   // Check if the user with the provided email exists
-  User.findOne({ email: req.body.email }, (err, user) => {
+  User.findOne({ email: req.body.email }, async (err, user) => {
     if (err) {
-      return res.status(500).send({ error: 'Error finding user' });
+      return res.status(500).send({ error: "Error finding user" });
     }
     if (!user) {
-      return res.status(404).send({ error: 'User not found' });
+      return res.status(404).send({ error: "User not found" });
     }
 
     // Generate a reset token and send it to the user's email
-    const resetToken = jwt.sign({ id: user._id }, process.env.RESET_TOKEN_SECRET, { expiresIn: '1h' });
-    sendResetEmail(user.email, resetToken);
+    const resetToken = jwt.sign(
+      { id: user._id },
+      process.env.RESET_TOKEN_SECRET,
+      { expiresIn: "1h" }
+    );
+    await sendResetEmail(user.email, resetToken);
+
+    // Save the reset password token in the database
+    user.resetPasswordToken = resetToken;
+    try {
+      await user.save();
+    } catch (error) {
+      return res
+        .status(500)
+        .send({ error: "Error saving reset password token" });
+    }
 
     // Return success message
-    res.send({ message: 'Reset email sent' });
-  });
+    res.send({ message: "Reset email sent" });
+  });
 };
--- controllers/authorisation/loginController.js
+++ controllers/authorisation/loginController.js
@@ -1,6 +1,8 @@
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = async function Login(req, res) {
   try {
